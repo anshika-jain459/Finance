088dfa51e41c6a704bc7f4531d2341fdfb1c41f2
anshika-jain459 <87385633+anshika-jain459@users.noreply.github.com>
2023-07-08 12:46:01 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,14 +1,12 @@
-import os
+import csv
+import datetime
+import pytz
 import requests
-import urllib.parse
+import subprocess
+import urllib
+import uuid
 
-# I have added this
-#import os
-import sys
-pipPath = f'{os.path.dirname(sys.executable)}\\Scripts'
-os.system(f'setx PATH "%PATH%;{pipPath}"')# this much only
-
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, session
 from functools import wraps
 
 
@@ -31,7 +29,7 @@ def login_required(f):
     """
     Decorate routes to require login.
 
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -39,6 +37,31 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+# def lookup(symbol):
+#     """Look up quote for symbol."""
+
+#     # Contact API
+#     try:
+#         api_key = os.environ.get("pk_1e93acf137424a61bc559476a2ca046e")
+#         # api_key = os.environ.get("API_KEY")
+#         # api_key = "pk_1e93acf137424a61bc559476a2ca046e"
+#         url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+#         response = requests.get(url)
+#         response.raise_for_status()
+#     except requests.RequestException:
+#         return None
+
+#     # Parse response
+#     try: # kind of dictionary
+#         quote = response.json()
+#         return {
+#             "name": quote["companyName"],
+#             "price": float(quote["latestPrice"]),
+#             "symbol": quote["symbol"]
+#         }
+#     except (KeyError, TypeError, ValueError):
+#         return None
 
 def lookup(symbol):
     """Look up quote for symbol."""
@@ -73,6 +96,54 @@ def lookup(symbol):
     except (requests.RequestException, ValueError, KeyError, IndexError):
         return None
 
+
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
+
+
+# import os
+# import requests
+# import urllib.parse
+
+# # I have added this
+# #import os
+# import sys
+# pipPath = f'{os.path.dirname(sys.executable)}\\Scripts'
+# os.system(f'setx PATH "%PATH%;{pipPath}"')# this much only
+
+# from flask import redirect, render_template, request, session
+# from functools import wraps
+
+
+# def apology(message, code=400):
+#     """Render message as an apology to user."""
+#     def escape(s):
+#         """
+#         Escape special characters.
+
+#         https://github.com/jacebrowning/memegen#special-characters
+#         """
+#         for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+#                          ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+#             s = s.replace(old, new)
+#         return s
+#     return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+# def login_required(f):
+#     """
+#     Decorate routes to require login.
+
+#     https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+#     """
+#     @wraps(f)
+#     def decorated_function(*args, **kwargs):
+#         if session.get("user_id") is None:
+#             return redirect("/login")
+#         return f(*args, **kwargs)
+#     return decorated_function
+
 # def lookup(symbol):
 #     """Look up quote for symbol."""
 
@@ -99,6 +170,6 @@ def lookup(symbol):
 #         return None
 
 
-def usd(value):
-    """Format value as USD."""
-    return f"${value:,.2f}"
+# def usd(value):
+#     """Format value as USD."""
+#     return f"${value:,.2f}"
