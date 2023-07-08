c064a47490c4c87ccaa45c38937da3baf64d3ed9
anshika-jain459 <87385633+anshika-jain459@users.noreply.github.com>
2023-07-08 12:40:56 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -40,31 +40,63 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-
 def lookup(symbol):
     """Look up quote for symbol."""
 
-    # Contact API
+    # Prepare API request
+    symbol = symbol.upper()
+    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
+    start = end - datetime.timedelta(days=7)
+
+    # Yahoo Finance API
+    url = (
+        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
+        f"?period1={int(start.timestamp())}"
+        f"&period2={int(end.timestamp())}"
+        f"&interval=1d&events=history&includeAdjustedClose=true"
+    )
+
+    # Query API
     try:
-        api_key = os.environ.get("pk_1e93acf137424a61bc559476a2ca046e")
-        # api_key = os.environ.get("API_KEY")
-        # api_key = "pk_1e93acf137424a61bc559476a2ca046e"
-        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
-        response = requests.get(url)
+        response = requests.get(url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
         response.raise_for_status()
-    except requests.RequestException:
+
+        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
+        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
+        quotes.reverse()
+        price = round(float(quotes[0]["Adj Close"]), 2)
+        return {
+            "name": symbol,
+            "price": price,
+            "symbol": symbol
+        }
+    except (requests.RequestException, ValueError, KeyError, IndexError):
         return None
 
-    # Parse response
-    try: # kind of dictionary
-        quote = response.json()
-        return {
-            "name": quote["companyName"],
-            "price": float(quote["latestPrice"]),
-            "symbol": quote["symbol"]
-        }
-    except (KeyError, TypeError, ValueError):
-        return None
+# def lookup(symbol):
+#     """Look up quote for symbol."""
+
+#     # Contact API
+#     try:
+#         api_key = os.environ.get("pk_1e93acf137424a61bc559476a2ca046e")
+#         # api_key = os.environ.get("API_KEY")
+#         # api_key = "pk_1e93acf137424a61bc559476a2ca046e"
+#         url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+#         response = requests.get(url)
+#         response.raise_for_status()
+#     except requests.RequestException:
+#         return None
+
+#     # Parse response
+#     try: # kind of dictionary
+#         quote = response.json()
+#         return {
+#             "name": quote["companyName"],
+#             "price": float(quote["latestPrice"]),
+#             "symbol": quote["symbol"]
+#         }
+#     except (KeyError, TypeError, ValueError):
+#         return None
 
 
 def usd(value):
